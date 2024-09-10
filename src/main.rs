0dbf8233c5a59216f9f372b5aec61eb04a802a5c
CJ Burkey <cjburkey01@gmail.com>
2024-09-10 19:24:43 -0400

    Work on loading screens

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
 mod game;
 
 use bevy::{diagnostic::FrameTimeDiagnosticsPlugin, prelude::*};
-use bevy_wasm_window_resize::WindowResizePlugin;
 use game::PlayMePlugin;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    fn hide_progress_screen(s: &str);
+}
 
 fn main() {
+    hide_progress_screen("");
+
     App::new()
         .add_plugins((
-            DefaultPlugins.set(ImagePlugin::default_nearest()),
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    canvas: Some("#main-canvas".to_string()),
+                    fit_canvas_to_parent: true,
+                    ..default()
+                }),
+                ..default()
+            }),
             FrameTimeDiagnosticsPlugin,
         ))
-        .add_plugins(WindowResizePlugin)
+        //.add_plugins(WindowResizePlugin)
         .add_plugins(PlayMePlugin)
         .run();
 }
